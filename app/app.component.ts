e20127fd8a6a416ef40d36ba33237f00e4449aac
Andrew Pappas <aragacalledpappas@gmail.com>
2016-06-30 10:21:52 -0400

    more boilerplate

---
+++ app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component }       from '@angular/core';
+
+@Component({
+  selector: 'food-sack',
+  template: `
+    <h1>Food Sack</h1>
+  `
+})
+
+export class AppComponent {
+  title = 'Food Sack'
+}
