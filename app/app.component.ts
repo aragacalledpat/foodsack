f0054ab5abfb51a9f7ef8d942beac18ca2172b22
Andrew Pappas <aragacalledpappas@gmail.com>
2016-07-21 11:01:19 -0400

    add links

--- app/app.component.ts
+++ app/app.component.ts
@@ -20,8 +20,8 @@ import { ROUTER_DIRECTIVES } from '@angular/router';
       <!-- Collect the nav links, forms, and other content for toggling -->
       <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
         <ul class="nav navbar-nav">
-          <li class="active"><a href="#">Recipes <span class="sr-only">(current)</span></a></li>
-          <li><a href="#">Ingredients</a></li>
+          <li class="active"><a routerLink="/foodsack">Recipes <span class="sr-only">(current)</span></a></li>
+          <li><a routerLink="/foodsack/ingredients">Ingredients</a></li>
           <li class="dropdown">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
             <ul class="dropdown-menu">
