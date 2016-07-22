12cc94f5749d46767a91aaaddb7b5f242a6f5e2a
Andrew Pappas <aragacalledpappas@gmail.com>
2016-07-22 14:03:35 -0400

    pop modal on add

--- app/add-recipe/add-recipe.component.ts
+++ app/add-recipe/add-recipe.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+/// <reference path="../../typings/globals/jquery/index.d.ts" />
+import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'add-recipe',
   template: `
-    <h1>please work</h1>
+  <div #addModal class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+          <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+        </div>
+        <div class="modal-body">
+          ...
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
+      </div>
+    </div>
+  </div>
   `
 })
 
-export class AddRecipeComponent {
-  
+export class AddRecipeComponent implements AfterViewInit{
+ @ViewChild('addModal') modal:ElementRef;
+   
+  ngAfterViewInit(){
+     $(this.modal.nativeElement).modal('show');
+  }
 }
