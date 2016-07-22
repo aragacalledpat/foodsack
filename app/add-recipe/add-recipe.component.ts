cb2c319207da39bee51eb87e972d63b5ce3a5d2f
Andrew Pappas <aragacalledpappas@gmail.com>
2016-07-22 15:12:04 -0400

    add form to add modal

--- app/add-recipe/add-recipe.component.ts
+++ app/add-recipe/add-recipe.component.ts
@@ -9,14 +9,19 @@ import { Router } from '@angular/router';
       <div class="modal-content">
         <div class="modal-header">
           <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-          <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+          <h4 class="modal-title" id="myModalLabel">Add Recipe</h4>
         </div>
         <div class="modal-body">
-          ...
+          <form>
+            <div class="form-group">
+              <label for="name">Name</label>
+              <input type="text" class="form-control" required>
+            </div>
+          </form>
         </div>
         <div class="modal-footer">
           <button type="button" class="btn btn-default" (click)="goHome()">Close</button>
-          <button type="button" class="btn btn-primary">Save changes</button>
+          <button type="submit" class="btn btn-primary">Add Recipe</button>
         </div>
       </div>
     </div>
