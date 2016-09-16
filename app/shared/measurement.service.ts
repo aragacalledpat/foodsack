abb635a22dce0235da2056f9a178ca4c516523cd
Andrew Pappas <aragacalledpappas@gmail.com>
2016-09-16 13:42:56 -0400

    add a few more measurements

--- app/shared/measurement.service.ts
+++ app/shared/measurement.service.ts
@@ -6,7 +6,13 @@ export class MeasurementService {
 
   getMeasurements()
   {
-    var Measurements:Measurement[] = [{id: 1, name: "None"},{id: 2, name: "Teaspoon"},{id:3, name: "Tablespoon"},{id: 4, name: "Cup"}];
+    var Measurements:Measurement[] = 
+    [{id: 1, name: "None"},
+    {id: 2, name: "Teaspoon"},
+    {id:3, name: "Tablespoon"},
+    {id: 4, name: "Cup"},
+    {id: 5, name: "Can"},
+  {id: 6, name: "Ounces"}];
     return Measurements;
   }
 }
